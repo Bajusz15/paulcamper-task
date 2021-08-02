dc63e416fdff5f87649992a8f6a576c822891dee
Máté Bajusz <bajszmate99@gmail.com>
2021-08-02 23:41:00 +0200

    refactor: independent test for cache translator

--- cached_translator_test.go
+++ cached_translator_test.go
@@ -4,12 +4,23 @@ import (
 	"context"
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"golang.org/x/text/language"
 	"testing"
 )
 
+type mockTranslatorForCache struct {
+	mock.Mock
+}
+
+func (mt *mockTranslatorForCache) Translate(ctx context.Context, from, to language.Tag, data string) (string, error) {
+	//return "mock translator result", nil
+	args := mt.Called(ctx, from, to, data)
+	return args.String(0), args.Error(1)
+}
+
 func Test_cachedTranslator_Translate(t *testing.T) {
-	m := new(mockTranslator)
+	m := new(mockTranslatorForCache)
 	c := context.TODO()
 	from := language.Hungarian
 	to := language.English
